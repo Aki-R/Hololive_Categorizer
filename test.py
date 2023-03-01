e99220361ad89a20103abc61d7942d21bb23787f
Aki-R <akiakr.s@gmail.com>
2023-03-01 23:22:48 +0900

    refactoring

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import cv2
 import glob
 import numpy as np
 from keras.models import load_model
-from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import load_img
 
 MODEL_PATH = './model.h5'
 PATH_TOKINOSORA = './Train_Data/tokinosora/*'
@@ -13,8 +13,8 @@ PATH_HUBUKI = './Train_Data/hubuki/*'
 files = glob.glob(PATH_PEKORA)
 file = random.choice(files)
 print(file)
-img = cv2.imread(file,1)
-cv2.imshow('original image',img)
+img = cv2.imread(file, 1)
+cv2.imshow('original image', img)
 
 model = load_model(MODEL_PATH)
 
@@ -22,11 +22,11 @@ image_pil = load_img(file, grayscale=False, color_mode='rgb', target_size=(256, 
 image = np.array(image_pil, dtype=np.uint8)
 image_flip = cv2.flip(image, 1)
 new_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-cv2.imshow('reshaped image',new_image)
+cv2.imshow('reshaped image', new_image)
 
 new_image_flip = cv2.cvtColor(image_flip, cv2.COLOR_RGB2BGR)
-cv2.imshow('flip image',new_image_flip)
-image_expand = image[np.newaxis, : ,: ,:]
+cv2.imshow('flip image', new_image_flip)
+image_expand = image[np.newaxis, :, :, :]
 print(image.shape)
 print(image_expand.shape)
 
