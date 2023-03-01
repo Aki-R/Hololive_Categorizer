b53206661e8f9b421c6c19bc55a450134855154e
Aki-R <akiakr.s@gmail.com>
2023-03-02 01:01:21 +0900

    use csv for vtuber name and train data path.

--- cateforizer_app.py
+++ cateforizer_app.py
@@ -3,67 +3,9 @@ from tkinter import ttk
 from keras.models import load_model
 from PIL import ImageGrab
 import numpy as np
+import pandas as pd
 
 MODEL_PATH = './model_app.h5'
-
-HOLOLIST = ['ときのそら',
-            'うさだぺこら',
-            '白上ふぶき',
-            '宝鐘マリン',
-            '白銀ノエル',
-            '星街すいせい',
-            '戌神ころね',
-            'さくらみこ',
-            '天音かなた',
-            'ロボ子さん',
-            '夜空メル',
-            'アキ・ローゼンタール',
-            '赤井はあと',
-            '夏色まつり',
-            '湊あくあ',
-            '紫咲シオン',
-            '百鬼あやめ',
-            '癒月ちょこ',
-            '大空スバル',
-            'AZKi',
-            '大神ミオ',
-            '猫又おかゆ',
-            '不知火フレア',
-            '角巻わため',
-            '常闇トワ',
-            '姫森ルーナ',
-            '雪花ラミィ',
-            '桃鈴ねね',
-            '獅白ぼたん',
-            '尾丸ポルカ',
-            'ラプラス・ダークネス',
-            '高嶺ルイ',
-            '博衣こより',
-            '沙花叉クロヱ',
-            '風真いろは',
-            'アユンダ・リス',
-            'ムーナ・ホシノヴァ',
-            'アイラニ・イオフィフティーン',
-            'クレイジー・オリー',
-            'アーニャ・メルフィッサ',
-            'パヴォリア・レイネ',
-            'ベスティア・ゼータ',
-            'カエラ・コヴァルスキア',
-            'こぼ・かなえる',
-            '森カリオペ',
-            '一伊那尓栖',
-            'がうる・ぐら',
-            'ワトソン・アメリア',
-            'IRyS',
-            '九十九佐命',
-            'セレス・ファウナ',
-            'オーロ・クロニー',
-            '七詩ムメイ',
-            'ハコス・ベールズ',
-            '桐生ココ',
-            '友人A',
-            '春先のどか',
-            '小鳥遊キアラ']
 
 # Global Variable
 image = {}
@@ -82,6 +24,7 @@ class Frame_Control(tk.Frame):
         self.create_wigdgets()
         # initialize Keras model
         self.model = load_model(MODEL_PATH)
+        self.df_vtuber = pd.read_csv("vtuber_list.csv")
 
     def create_wigdgets(self):
         # Start Button
@@ -96,8 +39,6 @@ class Frame_Control(tk.Frame):
         self.label_result.pack()
 
     def start_categorize(self):
-        print('start')
-        print(self.master.winfo_x())
         global image
         x1 = self.master.winfo_x() + 7
         x2 = x1+256
@@ -110,7 +51,8 @@ class Frame_Control(tk.Frame):
         result = self.model.predict(image_expand, batch_size=1)
         whois = result.argmax()
         probability = result[0, whois] * 100
-        result_text = f'{HOLOLIST[whois]}:{probability}, %'
+        name = self.df_vtuber[self.df_vtuber["id"] == whois]["name"].values[0]
+        result_text = f'{name}:{probability}, %'
         print(result_text)
         self.label_result.configure(text=result_text)
         self.after(100, self.start_categorize)
--- hololive_categorizer.py
+++ hololive_categorizer.py
@@ -12,127 +12,7 @@ from keras.preprocessing.image import load_img
 from keras.applications.vgg16 import VGG16
 from keras.callbacks import ModelCheckpoint
 from sklearn.model_selection import train_test_split
-
-# Categorize No.
-TOKINOSORA = 0
-PEKORA = 1
-HUBUKI = 2
-MARIN = 3
-NOEL = 4
-SUISEI = 5
-KORONE = 6
-MIKO = 7
-KANATA = 8
-ROBOCO = 9
-MEL = 10
-AKIROSE = 11
-HACHAMA = 12
-MATSURI = 13
-AQUA = 14
-SHION = 15
-AYAME = 16
-CHOCO = 17
-SUBARU = 18
-AZKI = 19
-MIO = 20
-OKAYU = 21
-FLARE = 22
-WATAME = 23
-TOWA = 24
-LUNA = 25
-LAMY = 26
-NENE = 27
-BOTAN = 28
-POLKA = 29
-LAPLUS = 30
-LUI = 31
-KOYORI = 32
-CHLOE = 33
-IROHA = 34
-RISU = 35
-MOONA = 36
-AIRANI = 37
-OLLIE = 38
-ANYA = 39
-REINE = 40
-ZETA = 41
-KAERA = 42
-KOBO = 43
-CALLIOPE = 44
-INANIS = 45
-GURA = 46
-AMELIA = 47
-IRYS = 48
-SANA = 49
-FAUNA = 50
-KRONII = 51
-MUMEI = 52
-BAELZ = 53
-COCO = 54
-FRIENDA = 55
-NODOKA = 56
-KIARA = 57
-
-
-# Image path
-PATH_TOKINOSORA = './Train_Data/tokinosora/*'
-PATH_PEKORA = './Train_Data/pekora/*'
-PATH_HUBUKI = './Train_Data/hubuki/*'
-PATH_MARIN = './Train_Data/marin/*'
-PATH_NOEL = './Train_Data/noel/*'
-PATH_SUISEI = './Train_Data/suisei/*'
-PATH_KORONE = './Train_Data/korone/*'
-PATH_MIKO = './Train_Data/miko/*'
-PATH_KANATA = './Train_Data/kanata/*'
-PATH_ROBOCO = './Train_Data/roboco/*'
-PATH_MEL = './Train_Data/mel/*'
-PATH_AKIROSE = './Train_Data/akirose/*'
-PATH_HACHAMA = './Train_Data/hachama/*'
-PATH_MATSURI = './Train_Data/matsuri/*'
-PATH_AQUA = './Train_Data/aqua/*'
-PATH_SHION = './Train_Data/shion/*'
-PATH_AYAME = './Train_Data/ayame/*'
-PATH_CHOCO = './Train_Data/choco/*'
-PATH_SUBARU = './Train_Data/subaru/*'
-PATH_AZUKI = './Train_Data/azuki/*'
-PATH_MIO = './Train_Data/mio/*'
-PATH_OKAYU = './Train_Data/okayu/*'
-PATH_FALRE = './Train_Data/flare/*'
-PATH_WATAME = './Train_Data/watame/*'
-PATH_TOWA = './Train_Data/towa/*'
-PATH_LUNA = './Train_Data/luna/*'
-PATH_LAMY = './Train_Data/lamy/*'
-PATH_NENE = './Train_Data/nene/*'
-PATH_BOTAN = './Train_Data/botan/*'
-PATH_POLKA = './Train_Data/polka/*'
-PATH_LAPLUS = './Train_Data/laplus/*'
-PATH_LUI = './Train_Data/lui/*'
-PATH_KOYORI = './Train_Data/koyori/*'
-PATH_CHLOE = './Train_Data/chloe/*'
-PATH_IROHA = './Train_Data/iroha/*'
-PATH_RISU = './Train_Data/risu/*'
-PATH_MOONA = './Train_Data/moona/*'
-PATH_AIRANI = './Train_Data/airani/*'
-PATH_OLLIE = './Train_Data/ollie/*'
-PATH_ANYA = './Train_Data/anya/*'
-PATH_REINE = './Train_Data/reine/*'
-PATH_ZETA = './Train_Data/zeta/*'
-PATH_KAERA = './Train_Data/kaera/*'
-PATH_KOBO = './Train_Data/kobo/*'
-PATH_CALLIOPE = './Train_Data/calliope/*'
-PATH_INANIS = './Train_Data/inanis/*'
-PATH_GURA = './Train_Data/gura/*'
-PATH_AMELIA = './Train_Data/amelia/*'
-PATH_IRYS = './Train_Data/irys/*'
-PATH_SANA = './Train_Data/sana/*'
-PATH_FAUNA = './Train_Data/fauna/*'
-PATH_KURONII = './Train_Data/kronii/*'
-PATH_MUMEI = './Train_Data/mumei/*'
-PATH_BAELZ = './Train_Data/baelz/*'
-PATH_COCO = './Train_Data/coco/*'
-PATH_FRIENDA = './Train_Data/frienda/*'
-PATH_NODOKA = './Train_Data/nodoka/*'
-PATH_KIARA = './Train_Data/kiara/*'
+import pandas as pd
 
 images = []
 names = []
@@ -190,67 +70,6 @@ def ImageLoad(number, path):
         names.append(name)
 
 
-def LoadAllImage():
-    ImageLoad(TOKINOSORA, PATH_TOKINOSORA)
-    ImageLoad(PEKORA, PATH_PEKORA)
-    ImageLoad(HUBUKI, PATH_HUBUKI)
-    ImageLoad(MARIN, PATH_MARIN)
-    ImageLoad(NOEL, PATH_NOEL)
-    ImageLoad(SUISEI, PATH_SUISEI)
-    ImageLoad(KORONE, PATH_KORONE)
-    ImageLoad(MIKO, PATH_MIKO)
-    ImageLoad(KANATA, PATH_KANATA)
-    ImageLoad(ROBOCO, PATH_ROBOCO)
-    ImageLoad(MEL, PATH_MEL)
-    ImageLoad(AKIROSE, PATH_AKIROSE)
-    ImageLoad(HACHAMA, PATH_HACHAMA)
-    ImageLoad(MATSURI, PATH_MATSURI)
-    ImageLoad(AQUA, PATH_AQUA)
-    ImageLoad(SHION, PATH_SHION)
-    ImageLoad(AYAME, PATH_AYAME)
-    ImageLoad(CHOCO, PATH_CHOCO)
-    ImageLoad(SUBARU, PATH_SUBARU)
-    ImageLoad(AZKI, PATH_AZUKI)
-    ImageLoad(MIO, PATH_MIO)
-    ImageLoad(OKAYU, PATH_OKAYU)
-    ImageLoad(FLARE, PATH_FALRE)
-    ImageLoad(WATAME, PATH_WATAME)
-    ImageLoad(TOWA, PATH_TOWA)
-    ImageLoad(LUNA, PATH_LUNA)
-    ImageLoad(LAMY, PATH_LAMY)
-    ImageLoad(NENE, PATH_NENE)
-    ImageLoad(BOTAN, PATH_BOTAN)
-    ImageLoad(POLKA, PATH_POLKA)
-    ImageLoad(LAPLUS, PATH_LAPLUS)
-    ImageLoad(LUI, PATH_LUI)
-    ImageLoad(KOYORI, PATH_KOYORI)
-    ImageLoad(CHLOE, PATH_CHLOE)
-    ImageLoad(IROHA, PATH_IROHA)
-    ImageLoad(RISU, PATH_RISU)
-    ImageLoad(MOONA, PATH_MOONA)
-    ImageLoad(AIRANI, PATH_AIRANI)
-    ImageLoad(OLLIE, PATH_OLLIE)
-    ImageLoad(ANYA, PATH_ANYA)
-    ImageLoad(REINE, PATH_REINE)
-    ImageLoad(ZETA, PATH_ZETA)
-    ImageLoad(KAERA, PATH_KAERA)
-    ImageLoad(KOBO, PATH_KOBO)
-    ImageLoad(CALLIOPE, PATH_CALLIOPE)
-    ImageLoad(INANIS, PATH_INANIS)
-    ImageLoad(GURA, PATH_GURA)
-    ImageLoad(AMELIA, PATH_AMELIA)
-    ImageLoad(IRYS, PATH_IRYS)
-    ImageLoad(SANA, PATH_SANA)
-    ImageLoad(FAUNA, PATH_FAUNA)
-    ImageLoad(KRONII, PATH_KURONII)
-    ImageLoad(MUMEI, PATH_MUMEI)
-    ImageLoad(BAELZ, PATH_BAELZ)
-    ImageLoad(COCO, PATH_COCO)
-    ImageLoad(FRIENDA, PATH_FRIENDA)
-    ImageLoad(NODOKA, PATH_NODOKA)
-    ImageLoad(KIARA, PATH_KIARA)
-
-
 if __name__ == "__main__":
     # Disable GPU
     # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
@@ -259,7 +78,10 @@ if __name__ == "__main__":
     print(tf.__version__)
     print(keras.__version__)
 
-    LoadAllImage()
+    df = pd.read_csv("vtuber_list.csv")
+
+    for index, row in df.iterrows():
+        ImageLoad(row["id"], row["path"])
 
     # Number to category array
     images_np = np.array(images)
